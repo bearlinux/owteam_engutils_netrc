3902ce117e2c9b74a5cf05f125f8120d5f3f7f6c
Bear Wadleigh <bwadleigh@glynlyon.com>
2014-07-08 02:01:09 -0700

    log updates, bump version

--- src/main/java/com/owteam/engUtils/EngLog.java
+++ src/main/java/com/owteam/engUtils/EngLog.java
@@ -20,8 +20,8 @@ import ch.qos.logback.core.rolling.SizeAndTimeBasedFNATP;
  * @author bwadleigh
  */
 public class EngLog {
-	
-	public static void clearAppenders(){
+
+	public static void clearAppenders() {
 		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 		root.detachAndStopAllAppenders();
 	}
@@ -29,31 +29,32 @@ public class EngLog {
 	public static void addFileAppender() {
 
 		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-		
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
 		root.setLevel(Level.TRACE);
 
 		PatternLayoutEncoder encoder = new PatternLayoutEncoder();
-		String logName="englog";
-		String logSize="10MB";
-		int logHistory=10;
+		encoder.setContext(loggerContext);
+		String logName = "englog";
+		String logSize = "10MB";
+		int logHistory = 10;
 		String logDir = System.getProperty("java.io.tmpdir");
-		
-		if(System.getProperty("script.name")!=null){
-			logName=System.getProperty("script.name");
+
+		if (System.getProperty("script.name") != null) {
+			logName = System.getProperty("script.name");
 		}
-		if(System.getProperty("englog.name")!=null){
-			logName=System.getProperty("englog.name");
+		if (System.getProperty("englog.name") != null) {
+			logName = System.getProperty("englog.name");
 		}
-		if(System.getProperty("englog.dir")!=null){
-			logDir=System.getProperty("englog.dir");
+		if (System.getProperty("englog.dir") != null) {
+			logDir = System.getProperty("englog.dir");
 		}
-		if(System.getProperty("englog.maxFileSize")!=null){
-			logDir=System.getProperty("englog.maxFileSize");
+		if (System.getProperty("englog.maxFileSize") != null) {
+			logDir = System.getProperty("englog.maxFileSize");
 		}
-		if(System.getProperty("englog.maxFiles")!=null){
-			logHistory=Integer.parseInt(System.getProperty("englog.maxFiles"));
+		if (System.getProperty("englog.maxFiles") != null) {
+			logHistory = Integer.parseInt(System.getProperty("englog.maxFiles"));
 		}
-		
+
 		encoder.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
 		encoder.start();
 
@@ -61,29 +62,32 @@ public class EngLog {
 		SizeAndTimeBasedFNATP sizePolicy = new SizeAndTimeBasedFNATP();
 		sizePolicy.setMaxFileSize("10MB");
 		rollPolicy.setTimeBasedFileNamingAndTriggeringPolicy(sizePolicy);
-		
-		rollPolicy.setFileNamePattern("/tmp/${dollar_zero}-%d{yyyy-MM-dd}.log");
+
+		rollPolicy.setFileNamePattern("/tmp/" + logName + "-%d{yyyy-MM-dd}.%i.log");
 		rollPolicy.setMaxHistory(3);
-		
 
 		RollingFileAppender rollingFileAppenderTrace = new RollingFileAppender();
-		rollingFileAppenderTrace.setName("${dollar_zero}-Log");
-		rollingFileAppenderTrace.setFile("/tmp/${dollar_zero}.log");
+		rollingFileAppenderTrace.setName(logName + "-Log");
+		rollingFileAppenderTrace.setFile("/tmp/" + logName + ".log");
 		rollingFileAppenderTrace.setEncoder(encoder);
 		rollingFileAppenderTrace.setRollingPolicy(rollPolicy);
 		rollPolicy.setParent(rollingFileAppenderTrace);
+		rollPolicy.setContext(loggerContext);
 		rollPolicy.start();
 		rollingFileAppenderTrace.start();
 		root.addAppender(rollingFileAppenderTrace);
 
 	}
+
 	public static void addConsoleAppender() {
 
-		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);	
+		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 		root.setLevel(Level.TRACE);
 		PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+		LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+		encoder.setContext(loggerContext);
 		encoder.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
-		encoder.start();		
+		encoder.start();
 		ConsoleAppender stderrAppender = new ConsoleAppender();
 		stderrAppender.setWithJansi(true);
 		stderrAppender.setEncoder(encoder);
